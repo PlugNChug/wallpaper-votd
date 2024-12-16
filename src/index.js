501bb308e48781e2543aeab62dd7ce9f9ae34a40
Julius Lee <juliuslee3929@gmail.com>
2024-12-16 02:10:36 -0600

    Functional VOTD implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import grabVerse from './verse'
-import './styles/main.scss'
-import HornPeak from './assets/HornPeak.jpg'
+import grabVerse from "./verse";
+import "./styles/main.scss";
+import HornPeak from "./assets/HornPeak.jpg";
 
-const mountainImg = document.getElementById('mountainImg');
+const mountainImg = document.getElementById("mountainImg");
 mountainImg.src = HornPeak;
 
 await grabVerse();
 
+// Modified version of w3schools' js clock example
+function clock() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById("clock").innerHTML = h + ":" + m;
+  setTimeout(clock, 1000);
+}
 
-console.log("Finished!");
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+clock();
+
+console.log("Everything should be on screen by now!");
--- src/verse.js
+++ src/verse.js
@@ -8,15 +8,27 @@ async function grabVerse() {
     console.log(link);
 
     // Get the first item in the further split parseableString to obtain the string for book and chapter
-    const chapter = parseableString.split('.')[0];
+    const location = parseableString.split('.')[0];
     // Get the second item in the further split parseableString to obtain a verse number string
     const verse = parseableString.split('.')[1];
 
-    console.log(chapter, verse);
+    console.log(location, verse);
 
-    axios.get(`https://bible-api.com/${chapter}:${verse}?translation=web`).then((res) => {
+    axios.get(`https://bible-api.com/${location}:${verse}?translation=web`).then((res) => {
       console.log(res.data.text);
       document.getElementById('verse').innerHTML = res.data.text;
+
+      var book = "NULL";
+      var chapter = "NULL";
+      var strIndex = 1;
+      while (!isNaN(location.substring(location.length - 1 - strIndex))) {
+        strIndex++;
+      }
+      console.log(strIndex);
+      book = location.substring(0, location.length - strIndex);
+      chapter = location.substring(location.length - strIndex);
+
+      document.getElementById('source').innerHTML = `${book} ${chapter}:${verse}`;
     })
   })
 }
