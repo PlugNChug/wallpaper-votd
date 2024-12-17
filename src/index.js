d46639276ad6018dbfb86895bd795ee1afe824b2
Julius Lee <juliuslee3929@gmail.com>
2024-12-17 02:49:04 -0600

    Background updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 import grabVerse from "./verse";
 import "./styles/main.scss";
 import HornPeak from "./assets/HornPeak.jpg";
+import BlueGradient from "./assets/BlueGradient.svg";
+import OrangeWaves from "./assets/OrangeWaves.svg";
 
-const mountainImg = document.getElementById("mountainImg");
-mountainImg.src = HornPeak;
+const nextBG = document.getElementById("background1");
+const currBG = document.getElementById("background2");
 
-await grabVerse();
+var prevSecond = -1;
+var grabCalled = false;
 
 // Modified version of w3schools' js clock example
 function clock() {
   const today = new Date();
   let h = today.getHours();
   let m = today.getMinutes();
+  let s = today.getSeconds();
+
+  // At each second, refresh background (TESTING PURPOSES ONLY)
+  if (prevSecond !== s) {
+    prevSecond = s;
+    setBG(today);
+  }
+
+  // At the top/bottom of each hour (XX:00:00 or XX:30:00), refresh the verse and background. A change should really only happen at midnight.
+  if (m % 30 === 0 && s === 0 && grabCalled) {
+    grabVerse();
+    grabCalled = false;
+  } else {
+    grabCalled = true;
+  }
+
+  // Convert values to displayable format
+  h = h % 12 === 0 ? 12 : h % 12;
   m = checkTime(m);
   document.getElementById("clock").innerHTML = h + ":" + m;
-  setTimeout(clock, 1000);
+
+  setTimeout(clock, 500);
 }
 
 function checkTime(i) {
@@ -24,6 +46,59 @@ function checkTime(i) {
   return i;
 }
 
+function setBG(today) {
+  let d = today.getSeconds();
+  switch (d % 7) {
+    case 0:
+      nextBG.src = BlueGradient;
+      currBG.classList.add("fade-out");
+      break;
+    case 1:
+      nextBG.src = OrangeWaves;
+      currBG.classList.add("fade-out");
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    default:
+      break;
+  }
+  setTimeout(() => {
+    currBG.src = nextBG.src;
+    currBG.classList.remove("fade-out");
+  }, 500);
+}
+
+function setInitialBG(today) {
+  let d = today.getSeconds();
+  switch (d % 3) {
+    case 0:
+      // Start fade out
+      currBG.src = BlueGradient;
+      nextBG.src = OrangeWaves;
+      break;
+    case 1:
+      currBG.src = OrangeWaves;
+      nextBG.src = HornPeak;
+      break;
+    case 2:
+      currBG.src = HornPeak;
+      nextBG.src = BlueGradient;
+      break;
+    default:
+      break;
+  }
+}
+
+setInitialBG(new Date());
 clock();
+await grabVerse();
 
 console.log("Everything should be on screen by now!");
--- src/verse.js
+++ src/verse.js
@@ -5,18 +5,13 @@ async function grabVerse() {
     // Obtain the link from the received data, then get the last portion of the link to obtain a preliminary string to parse further
     const link = res.data;
     const parseableString = link.split('/')[link.split('/').length - 1];
-    console.log(link);
 
     // Get the first item in the further split parseableString to obtain the string for book and chapter
     const location = parseableString.split('.')[0];
     // Get the second item in the further split parseableString to obtain a verse number string
     const verse = parseableString.split('.')[1];
 
-    console.log(location, verse);
-
     axios.get(`https://bible-api.com/${location}:${verse}?translation=web`).then((res) => {
-      console.log(res.data.text);
-      document.getElementById('verse').innerHTML = res.data.text;
 
       var book = "NULL";
       var chapter = "NULL";
@@ -24,10 +19,10 @@ async function grabVerse() {
       while (!isNaN(location.substring(location.length - 1 - strIndex))) {
         strIndex++;
       }
-      console.log(strIndex);
       book = location.substring(0, location.length - strIndex);
       chapter = location.substring(location.length - strIndex);
 
+      document.getElementById('verse').innerHTML = res.data.text;
       document.getElementById('source').innerHTML = `${book} ${chapter}:${verse}`;
     })
   })
