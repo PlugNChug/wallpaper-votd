1f9793fbe3f3ca53818eec0b0cc2a419d203ce70
Julius Lee <juliuslee3929@gmail.com>
2024-12-17 21:22:09 -0600

    More background improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import grabVerse from "./verse";
 import "./styles/main.scss";
 import HornPeak from "./assets/HornPeak.jpg";
+import NightSky from "./assets/NightSky.jpg";
 import BlueGradient from "./assets/BlueGradient.svg";
 import OrangeWaves from "./assets/OrangeWaves.svg";
+import GreenTiles from "./assets/GreenTiles.svg";
+import PinkWaves from "./assets/PinkWaves.svg";
 
 const nextBG = document.getElementById("background1");
 const currBG = document.getElementById("background2");
@@ -58,14 +61,22 @@ function setBG(today) {
       currBG.classList.add("fade-out");
       break;
     case 2:
+      nextBG.src = GreenTiles;
+      currBG.classList.add("fade-out");
       break;
     case 3:
+      nextBG.src = PinkWaves;
+      currBG.classList.add("fade-out");
       break;
     case 4:
       break;
     case 5:
+      nextBG.src = NightSky;
+      currBG.classList.add("fade-out");
       break;
     case 6:
+      nextBG.src = HornPeak;
+      currBG.classList.add("fade-out");
       break;
     default:
       break;
@@ -86,9 +97,25 @@ function setInitialBG(today) {
       break;
     case 1:
       currBG.src = OrangeWaves;
-      nextBG.src = HornPeak;
+      nextBG.src = GreenTiles;
       break;
     case 2:
+      currBG.src = GreenTiles;
+      nextBG.src = PinkWaves;
+      break;
+    case 3:
+      currBG.src = PinkWaves;
+      nextBG.src = BlueGradient;
+      break;
+    case 4:
+      currBG.src = GreenTiles;
+      nextBG.src = HornPeak;
+      break;
+    case 5:
+      currBG.src = GreenTiles;
+      nextBG.src = HornPeak;
+      break;
+    case 6:
       currBG.src = HornPeak;
       nextBG.src = BlueGradient;
       break;
